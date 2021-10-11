cdf47a0fcf6c95861dd676c9c317cf3b7ddeb798
Alex Withington-Smith <alex@awithsmith.com>
2021-10-11 06:28:18 +0100

    cart-dropdown and cart-item using styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,33 +4,37 @@ import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
 
 import CartItem from "../cart-item/cart-item.component";
-import CustomButton from "../custom-button/custom-button.component";
 
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-import "./cart-dropdown.styles.scss";
+import {
+  CartDropDownContainer,
+  CartItemsContainer,
+  EmptyMessageContainer,
+  ToggleCartButton,
+} from "./cart-dropdown.styles";
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
+  <CartDropDownContainer>
+    <CartItemsContainer>
       {cartItems.length ? (
         cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
         ))
       ) : (
-        <span className="empty-message">Your cart is empty</span>
+        <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
       )}
-    </div>
-    <CustomButton
+    </CartItemsContainer>
+    <ToggleCartButton
       onClick={() => {
         history.push("/checkout");
         dispatch(toggleCartHidden());
       }}
     >
       GO TO CHECKOUT
-    </CustomButton>
-  </div>
+    </ToggleCartButton>
+  </CartDropDownContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
-import "./cart-item.styles.scss";
+import {
+  CartItemContainer,
+  ItemImage,
+  NameContainer,
+  PriceContainer,
+  ItemDetailsContainer,
+} from "./cart-item.styles";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
+  <CartItemContainer>
+    <ItemImage src={imageUrl} alt="item" />
+    <ItemDetailsContainer>
+      <NameContainer>{name}</NameContainer>
+      <PriceContainer>
         {quantity} x £{price}
-      </span>
-    </div>
-  </div>
+      </PriceContainer>
+    </ItemDetailsContainer>
+  </CartItemContainer>
 );
 
 export default CartItem;
