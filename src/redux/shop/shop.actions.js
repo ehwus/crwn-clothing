394e55950824231d67de3a4956eb0c520a1e9edf
Alex Withington-Smith <alex@awithsmith.com>
2021-10-23 12:13:10 +0100

    added container pattern and beginnings of redux sagas for side effects

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,35 @@
 import ShopActionTypes from "./shop.types";
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/firebase.utils";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = (errorMsg) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMsg,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    dispatch(fetchCollectionsStart());
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        dispatch(
+          fetchCollectionsSuccess(convertCollectionsSnapshotToMap(snapshot))
+        );
+      })
+      .catch((e) => dispatch(fetchCollectionsFailure(e.message)));
+  };
+};
